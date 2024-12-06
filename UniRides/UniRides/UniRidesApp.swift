024402edd55a71149643127f7e6199b825c675b3
Benjamin Nudelman <bnudelman2@gmail.com>
2024-12-06 13:33:15 -0500

    added packages

---
+++ UniRides/UniRides/UniRidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniRidesApp.swift
+//  UniRides
+//
+//  Created by Benjamin Nudelman on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct UniRidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
