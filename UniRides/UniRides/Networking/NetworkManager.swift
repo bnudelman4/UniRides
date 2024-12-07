10ab3eb5c5d1cd9711a78cd6cc8fe9dc11691b22
Benjamin Nudelman <bnudelman2@gmail.com>
2024-12-06 19:15:03 -0500

    added progress

--- UniRides/UniRides/Networking/NetworkManager.swift
+++ UniRides/UniRides/Networking/NetworkManager.swift
@@ -1,7 +1,36 @@
 //
 //  NetworkManager.swift
-//  UniRides
-//
-//  Created by Benjamin Nudelman on 12/6/24.
 //
 
+import Alamofire
+import Foundation
+
+class NetworkManager {
+
+    /// Shared singleton instance
+    static let shared = NetworkManager()
+
+    private init() { }
+    
+    //decoder
+    let decoder = JSONDecoder()
+//
+    // MARK: - Requests
+    func fetchAllRides(completion: @escaping ([Car]) -> Void){
+        let devEndpoint: String = "https://api.jsonbin.io/v3/b/64d033f18e4aa6225ecbcf9f?meta=false"
+        decoder.dateDecodingStrategy = .iso8601
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        AF.request(devEndpoint, method: .get).validate().responseDecodable(of: [Car].self, decoder: decoder){
+            response in switch response.result {
+            case .success(let rides):
+                print("Successfully fetched rides: \(rides)")
+                completion(recipes)
+            case .failure(let error):
+                print("Error fetching rides: \(error)")
+            }
+        }
+        
+    }
+    
+}
--- UniRides/UniRides/views/FilterCollectionViewCell.swift
+++ UniRides/UniRides/views/FilterCollectionViewCell.swift
@@ -1,8 +1,46 @@
 //
 //  FilterCollectionViewCell.swift
-//  UniRides
 //
-//  Created by Benjamin Nudelman on 12/6/24.
-//
+import UIKit
+import SnapKit
+import SDWebImage
 
-import Foundation
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Properties
+    let filterButton = UIButton()
+
+    static let reuse = "FilterCollectionViewCellReuse"
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupFilterButton()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+    func configure(with title: String, isSelected: Bool) {
+        filterButton.setTitle(title, for: .normal)
+        filterButton.backgroundColor = isSelected ? UIColor.systemOrange.withAlphaComponent(0.8) : .systemGray6
+        filterButton.setTitleColor(isSelected ? .white : .black, for: .normal)
+    }
+
+    // MARK: - Setup
+    private func setupFilterButton() {
+            filterButton.layer.cornerRadius = 16
+            filterButton.layer.masksToBounds = true
+            filterButton.backgroundColor = .white
+            filterButton.setTitleColor(.black, for: .normal)
+            filterButton.titleLabel?.font = .systemFont(ofSize: 12, weight: .medium)
+            filterButton.isUserInteractionEnabled = false
+            contentView.addSubview(filterButton)
+            
+            filterButton.snp.makeConstraints{ make in
+                make.edges.equalToSuperview()
+            }
+        }
+}
